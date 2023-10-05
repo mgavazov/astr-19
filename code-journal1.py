ebad3af3d2fe77a8e0111a31f9a925945e74fc91
mgavazov <146883035+mgavazov@users.noreply.github.com>
2023-10-04 18:06:38 -0700

    Add files via upload

---
+++ code-journal1.py
@@ -0,0 +1,17 @@
+#This program will be my
+#introduction for my code journal
+
+#This function tells the
+#computer to print my introduction
+def PrintIamMaggieGavazovIlikeTwilightbecausewhynotandPersianfoodbecauseitisSOYUMMY():
+	print("I am Maggie Gavazov. I like Twilight because why not and Persian food because it is SO YUMMY!")
+
+
+#This defines our main()
+#function for our program
+def main():
+	PrintIamMaggieGavazovIlikeTwilightbecausewhynotandPersianfoodbecauseitisSOYUMMY()
+
+
+if __name__ == '__main__':
+	main()
