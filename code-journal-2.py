9ac20182311b04755dc79373a419d830459720b7
mgavazov <146883035+mgavazov@users.noreply.github.com>
2023-10-10 14:22:44 -0700

    Add files via upload

---
+++ code-journal-2.py
@@ -0,0 +1,23 @@
+#This program will print
+#The sum of two floating point numbers
+
+x = float(2.4)
+y = float(3.6)
+
+print("x+y = ", x+y)
+
+#This program will print
+#The difference between two integers
+
+x = 7
+y = 2
+
+print("x-y = ", x-y)
+
+#This program will print
+#The product between a floating point number and an integer
+
+x = float(10)
+y = 4
+
+print("x*y = ", x*y)
