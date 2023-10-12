f56091431236a459250a1c30166e92db85afa778
mgavazov <146883035+mgavazov@users.noreply.github.com>
2023-10-12 11:24:03 -0700

    Add files via upload

---
+++ code-journal-3.py
@@ -0,0 +1,13 @@
+#This program will print out a function f(x)
+def function(x):
+	return x**3 + 8 
+
+#The imput of the function will be 9
+result = function(9)
+print(result)
+
+#If the result is greater than 27
+#It will print out YAY!
+x = 27
+if result > x:
+	print("YAY!")
