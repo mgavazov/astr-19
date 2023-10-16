a60928328b571160e138cf9e77794f57fdc6110c
mgavazov <146883035+mgavazov@users.noreply.github.com>
2023-10-15 17:31:48 -0700

    Add files via upload

--- code-journal-3.py
+++ code-journal-3.py
@@ -1,13 +1,17 @@
 #This program will print out a function f(x)
-def function(x):
-	return x**3 + 8 
 
-#The imput of the function will be 9
-result = function(9)
-print(result)
+def f(x):
+	return x**3 + 8
 
-#If the result is greater than 27
-#It will print out YAY!
-x = 27
-if result > x:
-	print("YAY!")
+#The input of the function will be 9
+
+def main():
+	x = 9
+	result = f(x)
+	print(result)
+
+	if result > 27:
+		print("YAY!")
+
+if __name__ == '__main__':
+	main()
