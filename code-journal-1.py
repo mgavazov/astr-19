152a50d47df8c56e276aeb4187448cbe54c6e49b
mgavazov <146883035+mgavazov@users.noreply.github.com>
2023-10-19 13:26:52 -0700

    Add files via upload

---
+++ code-journal-1.py
@@ -0,0 +1,10 @@
+#This program will be my intro 
+
+def main():
+	print("I am Maggie Gavazov.")
+	print("My favorite movie is Twilight because it is so good and funny to watch")
+	print("I also love Persion food because of all the different flavors and spices!")
+	print("SO YUMMY!!!")
+
+if __name__ == '__main__':
+	main()
